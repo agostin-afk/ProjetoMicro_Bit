af087a4ffb53f0256a13760d6100d5f0b4e6995f
agostin-afk <agostinho008.af45@gmail.com>
2023-01-09 23:01:13 -0300

    modified:   Bussola/Cod.py

--- Bussola/Cod.py
+++ Bussola/Cod.py
@@ -1,3 +1,40 @@
+letra = ''
+
+def Pins(letra):
+    if letra == 'N':
+        pins.digital_write_pin(DigitalPin.P1, 1)
+        basic.pause(500)
+        pins.digital_write_pin(DigitalPin.P1, 0)
+    elif letra == 'NE':
+        pins.digital_write_pin(DigitalPin.P2, 1)
+        basic.pause(500)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+    elif letra == 'SE':
+        pins.digital_write_pin(DigitalPin.P3, 1)
+        basic.pause(500)
+        pins.digital_write_pin(DigitalPin.P3, 0)
+    elif letra == 'L':
+        pins.digital_write_pin(DigitalPin.P4, 1)
+        basic.pause(500)
+        pins.digital_write_pin(DigitalPin.P4, 0)
+    elif letra == 'S':
+        pins.digital_write_pin(DigitalPin.P5, 1)
+        basic.pause(500)
+        pins.digital_write_pin(DigitalPin.P5, 0)
+    elif letra == 'SO':
+        pins.digital_write_pin(DigitalPin.P6, 1)
+        basic.pause(500)
+        pins.digital_write_pin(DigitalPin.P6, 0)
+    elif letra == 'O':
+        pins.digital_write_pin(DigitalPin.P7, 1)
+        basic.pause(500)
+        pins.digital_write_pin(DigitalPin.P7, 0)
+    else:
+        pins.digital_write_pin(DigitalPin.P8, 1)
+        basic.pause(500)
+        pins.digital_write_pin(DigitalPin.P8, 0)
+        
+
 def on_button_pressed_b():
     global pontos
     while True:
@@ -6,106 +43,53 @@ def on_button_pressed_b():
             break
         pontos = input.compass_heading()
         if pontos < 22 or pontos > 337:
-            pins.digital_write_pin(DigitalPin.P1, 1)
-            pins.digital_write_pin(DigitalPin.P2, 0)
-            pins.digital_write_pin(DigitalPin.P3, 0)
-            pins.digital_write_pin(DigitalPin.P4, 0)
-            pins.digital_write_pin(DigitalPin.P5, 0)
-            pins.digital_write_pin(DigitalPin.P6, 0)
-            pins.digital_write_pin(DigitalPin.P7, 0)
-            pins.digital_write_pin(DigitalPin.P8, 0)
-            basic.show_string("N")
+            letra = 'N'
+            basic.show_string(letra)
+            Pins(letra)
         else:
             basic.clear_screen()
             if pontos > 67 or pontos < 23:
                 basic.clear_screen()
             else:
-                pins.digital_write_pin(DigitalPin.P1, 0)
-                pins.digital_write_pin(DigitalPin.P2, 1)
-                pins.digital_write_pin(DigitalPin.P3, 0)
-                pins.digital_write_pin(DigitalPin.P4, 0)
-                pins.digital_write_pin(DigitalPin.P5, 0)
-                pins.digital_write_pin(DigitalPin.P6, 0)
-                pins.digital_write_pin(DigitalPin.P7, 0)
-                pins.digital_write_pin(DigitalPin.P8, 0)
-                basic.show_string("NE")
+                letra = 'NE'
+                basic.show_string(letra)
+                Pins(letra)
         if pontos < 113 or pontos > 157:
             basic.clear_screen()
         else:
-            pins.digital_write_pin(DigitalPin.P1, 0)
-            pins.digital_write_pin(DigitalPin.P2, 0)
-            pins.digital_write_pin(DigitalPin.P3, 1)
-            pins.digital_write_pin(DigitalPin.P4, 0)
-            pins.digital_write_pin(DigitalPin.P5, 0)
-            pins.digital_write_pin(DigitalPin.P6, 0)
-            pins.digital_write_pin(DigitalPin.P7, 0)
-            pins.digital_write_pin(DigitalPin.P8, 0)
-            basic.show_string("SE")
+            letra = 'SE'
+            basic.show_string(letra)
+            Pins(letra)
         if pontos < 68 or pontos > 112:
             basic.clear_screen()
         else:
-            pins.digital_write_pin(DigitalPin.P1, 0)
-            pins.digital_write_pin(DigitalPin.P2, 0)
-            pins.digital_write_pin(DigitalPin.P3, 0)
-            pins.digital_write_pin(DigitalPin.P4, 1)
-            pins.digital_write_pin(DigitalPin.P5, 0)
-            pins.digital_write_pin(DigitalPin.P6, 0)
-            pins.digital_write_pin(DigitalPin.P7, 0)
-            pins.digital_write_pin(DigitalPin.P8, 0)
-            basic.show_string("L")
-            pins.digital_write_pin(DigitalPin.P0, 0)
+            letra = 'L'
+            basic.show_string(letra)
+            Pins(letra)
         if pontos < 158 or pontos > 202:
             basic.clear_screen()
         else:
-            pins.digital_write_pin(DigitalPin.P1, 0)
-            pins.digital_write_pin(DigitalPin.P2, 0)
-            pins.digital_write_pin(DigitalPin.P3, 0)
-            pins.digital_write_pin(DigitalPin.P4, 0)
-            pins.digital_write_pin(DigitalPin.P5, 1)
-            pins.digital_write_pin(DigitalPin.P6, 0)
-            pins.digital_write_pin(DigitalPin.P7, 0)
-            pins.digital_write_pin(DigitalPin.P8, 0)
-            basic.show_string("S")
-        pins.digital_write_pin(DigitalPin.P0, 0)
+            letra = 'S'
+            basic.show_string(letra)
+            Pins(letra)
         if pontos < 203 or pontos > 247:
             basic.clear_screen()
         else:
-            pins.digital_write_pin(DigitalPin.P1, 0)
-            pins.digital_write_pin(DigitalPin.P2, 0)
-            pins.digital_write_pin(DigitalPin.P3, 0)
-            pins.digital_write_pin(DigitalPin.P4, 0)
-            pins.digital_write_pin(DigitalPin.P5, 0)
-            pins.digital_write_pin(DigitalPin.P6, 1)
-            pins.digital_write_pin(DigitalPin.P7, 0)
-            pins.digital_write_pin(DigitalPin.P8, 0)
-            basic.show_string("SO")
-            pins.digital_write_pin(DigitalPin.P0, 0)
+            letra = 'SO'
+            basic.show_string(letra)
+            Pins(letra)
         if pontos < 248 or pontos > 292:
             basic.clear_screen()
         else:
-            pins.digital_write_pin(DigitalPin.P1, 0)
-            pins.digital_write_pin(DigitalPin.P2, 0)
-            pins.digital_write_pin(DigitalPin.P3, 0)
-            pins.digital_write_pin(DigitalPin.P4, 0)
-            pins.digital_write_pin(DigitalPin.P5, 0)
-            pins.digital_write_pin(DigitalPin.P6, 0)
-            pins.digital_write_pin(DigitalPin.P7, 1)
-            pins.digital_write_pin(DigitalPin.P8, 0)
-            basic.show_string("O")
-            pins.digital_write_pin(DigitalPin.P0, 0)
+            letra = 'O'
+            basic.show_string(letra)
+            Pins(letra)
         if pontos < 293 or pontos > 336:
             basic.clear_screen()
         else:
-            pins.digital_write_pin(DigitalPin.P1, 0)
-            pins.digital_write_pin(DigitalPin.P2, 0)
-            pins.digital_write_pin(DigitalPin.P3, 0)
-            pins.digital_write_pin(DigitalPin.P4, 0)
-            pins.digital_write_pin(DigitalPin.P5, 0)
-            pins.digital_write_pin(DigitalPin.P6, 0)
-            pins.digital_write_pin(DigitalPin.P7, 0)
-            pins.digital_write_pin(DigitalPin.P8, 1)
-            basic.show_string("NO")
-            pins.digital_write_pin(DigitalPin.P0, 0)
+            letra = 'NO'
+            basic.show_string(letra)
+            Pins(letra)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 passo = 0
