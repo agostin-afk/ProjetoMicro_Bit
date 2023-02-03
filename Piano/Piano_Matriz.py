9cabdc540bfb0d7d156e40b5d17595298b8649c9
agostin-afk <agostinho008.af45@gmail.com>
2023-02-03 14:12:47 -0300

    Update Piano_Matriz.py

--- Piano/Piano_Matriz.py
+++ Piano/Piano_Matriz.py
@@ -1,35 +1,30 @@
-# Piano_Matriz
-tecla_0 = 0
-tecla_1 = 0
-tecla_2 = 0
-
+line0 = 0
+line1 = 0
+line2 = 0
 
 def on_forever():
-    global tecla_0, tecla_1, tecla_2
-    tecla_0 = pins.digital_read_pin(DigitalPin.P4)
-    tecla_1 = pins.digital_read_pin(DigitalPin.P1)
-    tecla_2 = pins.digital_read_pin(DigitalPin.P2)
-    if tecla_0 and (not (tecla_1) and not (tecla_2)):
-        music.set_tempo(120)
-        music.play_tone(264, music.beat(BeatFraction.HALF))
-    if tecla_1 and (not (tecla_0) and not (tecla_2)):
-        music.set_tempo(120)
-        music.play_tone(297, music.beat(BeatFraction.HALF))
-    if tecla_2 and (not (tecla_0) and not (tecla_1)):
-        music.set_tempo(120)
-        music.play_tone(330, music.beat(BeatFraction.HALF))
-    if not (tecla_2) and (tecla_0 and tecla_1):
-        music.set_tempo(120)
-        music.play_tone(352, music.beat(BeatFraction.HALF))
-    if not (tecla_0) and (tecla_1 and tecla_2):
-        music.set_tempo(120)
-        music.play_tone(396, music.beat(BeatFraction.HALF))
-    if not (tecla_1) and (tecla_0 and tecla_2):
-        music.set_tempo(120)
-        music.play_tone(440, music.beat(BeatFraction.HALF))
-    if tecla_0 and (tecla_1 and tecla_2):
-        music.set_tempo(120)
-        music.play_tone(495, music.beat(BeatFraction.HALF))
-
-
+    global line0, line1, line2
+    line0 = pins.digital_read_pin(DigitalPin.P2)
+    line1 = pins.digital_read_pin(DigitalPin.P8)
+    line2 = pins.digital_read_pin(DigitalPin.P1)
+    if line0 or (line1 or line2):
+        basic.pause(30)
+        line0 = pins.digital_read_pin(DigitalPin.P2)
+        line1 = pins.digital_read_pin(DigitalPin.P8)
+        line2 = pins.digital_read_pin(DigitalPin.P1)
+        if not (line2) and (not (line1) and line0):
+            music.play_tone(262, music.beat(BeatFraction.HALF))
+        if not (line2) and (line1 and not (line0)):
+            music.play_tone(294, music.beat(BeatFraction.HALF))
+        if not (line2) and (line1 and line0):
+            music.play_tone(330, music.beat(BeatFraction.HALF))
+        if line2 and (not (line1) and not (line0)):
+            music.play_tone(349, music.beat(BeatFraction.HALF))
+        if line2 and (not (line1) and line0):
+            music.play_tone(392, music.beat(BeatFraction.HALF))
+        if line2 and (line1 and not (line0)):
+            music.play_tone(440, music.beat(BeatFraction.HALF))
+        if line2 and (line1 and line0):
+            music.play_tone(494, music.beat(BeatFraction.HALF))
 basic.forever(on_forever)
+on_forever()
