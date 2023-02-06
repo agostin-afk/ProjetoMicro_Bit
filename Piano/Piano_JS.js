74a45777368024958060d15861d4b59ee1219fb2
agostin-afk <agostinho008.af45@gmail.com>
2023-02-06 09:43:00 -0300

    ajustes

--- Piano/Piano_JS.js
+++ Piano/Piano_JS.js
@@ -1,44 +1,51 @@
 let pin01 = 0
 let pin02 = 0
-let pin08 = 0
+let pin00 = 0
 function notas() {
     while (true) {
         basic.pause(0)
+        pin00 = pins.digitalReadPin(DigitalPin.P0)
         pin01 = pins.digitalReadPin(DigitalPin.P1)
         pin02 = pins.digitalReadPin(DigitalPin.P2)
-        pin08 = pins.digitalReadPin(DigitalPin.P0)
-        if (!pin01 && (!pin02 && pin08)) {
-            music.playTone(262, music.beat(BeatFraction.Half))
+        if (pin00 && (!pin02 && !pin01)) {
+            music.playTone(264, music.beat(BeatFraction.Half))
+            //  Do
             notas()
         }
         
-        if (!pin01 && (pin02 && !pin08)) {
-            music.playTone(294, music.beat(BeatFraction.Half))
+        if (!pin02 && (pin01 && !pin00)) {
+            music.playTone(297, music.beat(BeatFraction.Half))
+            //  Ré
             notas()
         }
         
-        if (!pin01 && (pin02 && pin08)) {
+        if (pin02 && (!pin01 && !pin00)) {
             music.playTone(330, music.beat(BeatFraction.Half))
+            //  Mí
             notas()
         }
         
-        if (pin01 && (!pin02 && !pin08)) {
-            music.playTone(349, music.beat(BeatFraction.Half))
+        if (!pin02 && (pin01 && pin00)) {
+            music.playTone(352, music.beat(BeatFraction.Half))
+            //  Fá
             notas()
         }
         
-        if (pin01 && (!pin02 && pin08)) {
-            music.playTone(392, music.beat(BeatFraction.Half))
+        if (pin01 && (pin02 && !pin00)) {
+            music.playTone(396, music.beat(BeatFraction.Half))
+            //  Sol
             notas()
         }
         
-        if (pin01 && (pin02 && !pin08)) {
+        if (pin02 && (pin00 && !pin01)) {
             music.playTone(440, music.beat(BeatFraction.Half))
+            //  Lá
             notas()
         }
         
-        if (pin01 && (pin02 && pin08)) {
-            music.playTone(494, music.beat(BeatFraction.Half))
+        if (pin01 && (pin02 && pin00)) {
+            music.playTone(495, music.beat(BeatFraction.Half))
+            //  Si
             notas()
         }
         
@@ -50,7 +57,7 @@ basic.forever(function on_forever() {
     
     pin01 = pins.digitalReadPin(DigitalPin.P1)
     pin02 = pins.digitalReadPin(DigitalPin.P2)
-    pin08 = pins.digitalReadPin(DigitalPin.P0)
+    pin00 = pins.digitalReadPin(DigitalPin.P0)
     if (input.buttonIsPressed(Button.AB)) {
         notas()
         music.playTone(0, music.beat(BeatFraction.Half))
--- Piano/Piano_py.py
+++ Piano/Piano_py.py
@@ -1,39 +1,47 @@
 pin01 = 0
 pin02 = 0
-pin08 = 0
+pin00 = 0
 def notas():
     while True:
         basic.pause(0)
+        pin00 = pins.digital_read_pin(DigitalPin.P0)
         pin01 = pins.digital_read_pin(DigitalPin.P1)
         pin02 = pins.digital_read_pin(DigitalPin.P2)
-        pin08 = pins.digital_read_pin(DigitalPin.P0)
-        if not (pin01) and (not (pin02) and pin08):
-            music.play_tone(262, music.beat(BeatFraction.HALF))
+        if (pin00) and (not (pin02) and  not (pin01)):
+            music.play_tone(264, music.beat(BeatFraction.HALF))
+            # Do
             notas()
-        if not (pin01) and (pin02 and not (pin08)):
-            music.play_tone(294, music.beat(BeatFraction.HALF))
+        if not (pin02) and (pin01 and not (pin00)):
+            music.play_tone(297, music.beat(BeatFraction.HALF))
+            # Ré
             notas()
-        if not (pin01) and (pin02 and pin08):
+        if pin02 and (not (pin01) and not (pin00)):
             music.play_tone(330, music.beat(BeatFraction.HALF))
+            # Mí
             notas()
-        if pin01 and (not (pin02) and not (pin08)):
-            music.play_tone(349, music.beat(BeatFraction.HALF))
+        if not (pin02) and (pin01 and pin00):
+            music.play_tone(352, music.beat(BeatFraction.HALF))
+            # Fá
             notas()
-        if pin01 and (not (pin02) and pin08):
-            music.play_tone(392, music.beat(BeatFraction.HALF))
+        if pin01 and ((pin02) and not (pin00)):
+            music.play_tone(396, music.beat(BeatFraction.HALF))
+            # Sol
             notas()
-        if pin01 and (pin02 and not (pin08)):
+        if pin02 and (pin00 and not (pin01)):
             music.play_tone(440, music.beat(BeatFraction.HALF))
+            # Lá
             notas()
-        if pin01 and (pin02 and pin08):
-            music.play_tone(494, music.beat(BeatFraction.HALF))
+        if pin01 and (pin02 and pin00):
+            music.play_tone(495, music.beat(BeatFraction.HALF))
+            # Si
             notas()
         music.play_tone(0, music.beat(BeatFraction.HALF))
+
 def on_forever():
-    global pin01, pin02, pin08
+    global pin01, pin02, pin00
     pin01 = pins.digital_read_pin(DigitalPin.P1)
     pin02 = pins.digital_read_pin(DigitalPin.P2)
-    pin08 = pins.digital_read_pin(DigitalPin.P0)
+    pin00 = pins.digital_read_pin(DigitalPin.P0)
     if input.button_is_pressed(Button.AB):
         notas()
         music.play_tone(0, music.beat(BeatFraction.HALF))
